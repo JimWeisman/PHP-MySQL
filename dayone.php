cf2bc627c0e90faaa270de4a76991f19cca6db3e
Jim Weisman <jweisman@students.mchenry.edu>
2020-01-25 13:34:38 -0600

    Add files via upload

---
+++ dayone.php
@@ -0,0 +1,22 @@
+<html>
+<head><meta http-equiv="Content-Type" contnet="text/html; charset=uft-8'>
+</head
+
+<body>
+<div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+    
+<div id="example">Example 1-1</div>
+
+<div id="content">
+ <h1> this is good practice at hand coding., 
+    <?php
+ $name = $_REQUEST['name'];
+ echo " " . $name . " "?>
+ </h1>
+ 
+ <p> I am a slow typer</p>
+ </form>
+ </div>
+ div id="footer"></div>
+</body>
+</html>
